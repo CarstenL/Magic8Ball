f194da446150ec9f17cf354122a7120f6102ee11
CarstenL <c.lockenkoetter@web.de>
2013-08-17 22:18:12 +0200

    Klassen zur Verbindung und Abfragen der Datenbank hinzugefügt; DatabaseHelper wenns geht nicht mehr anpacken!! Datenbank-Abfragen in DataAdapter hinterlegen

---
+++ Magic8Ball/src/main/java/com/magic8ball/DataAdapter.java
@@ -0,0 +1,82 @@
+package com.magic8ball;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DataAdapter {
+    protected static final String TAG = "DataAdapter";
+    private final Context mContext;
+    private SQLiteDatabase mDb;
+    private DatabaseHelper mDbHelper;
+
+    public DataAdapter(Context context) {
+        this.mContext = context;
+        mDbHelper = new DatabaseHelper(mContext);
+    }
+
+    public DataAdapter createDatabase() throws SQLException {
+        try {
+            mDbHelper.createDataBase();
+        } catch (IOException mIOException) {
+            Toast.makeText(mContext, "Verbindung zur Datenbank fehlgeschlagen", Toast.LENGTH_LONG).show();
+        }
+        return this;
+    }
+
+    public DataAdapter open() throws SQLException {
+        try {
+            mDbHelper.openDataBase();
+            mDbHelper.close();
+            mDb = mDbHelper.getReadableDatabase();
+        } catch (SQLException mSQLException) {
+            Toast.makeText(mContext, "Datenbank-Verbindungsprobleme", Toast.LENGTH_LONG).show();
+        }
+        return this;
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+    public Cursor getAllGroups() {
+        try {
+            String sql = "Select NAME from GROUPS order by NAME";
+            Cursor mCur = mDb.rawQuery(sql, null);
+            if (mCur != null) {
+                mCur.moveToFirst();
+            }
+            return mCur;
+        } catch (SQLException ex) {
+            Toast.makeText(mContext, "Fehler beim Ermitteln aller Gruppen", Toast.LENGTH_LONG).show();
+            return null;
+        }
+    }
+
+    public Cursor getClaims(ArrayList<String> groups) {
+        try {
+            String whereClause = "";
+            for (String group : groups) {
+                if (whereClause == "")
+                    whereClause = "NAME like '" + group + "'";
+                else
+                    whereClause += " and NAME like '" + group + "'";
+            }
+
+            String sql = "Select VALUE from CLAIM c inner join CLAIM_GROUP cg on c.id = cg.id_claim inner join groups g on cg.id_group = g.id where " + whereClause;
+            Cursor mCur = mDb.rawQuery(sql, null);
+            if (mCur != null)
+                mCur.moveToFirst();
+
+            return mCur;
+        } catch (SQLException ex) {
+            Toast.makeText(mContext, "Fehler beim Ermitteln der Sprüche", Toast.LENGTH_LONG).show();
+            return null;
+        }
+    }
+}
---
+++ Magic8Ball/src/main/java/com/magic8ball/DatabaseHelper.java
@@ -0,0 +1,116 @@
+package com.magic8ball;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+    private static String TAG = "DataBaseHelper"; // Tag just for the LogCat window
+    //destination path (location) of our database on device
+    private static String DB_PATH = "";
+    private static String DB_NAME ="magic8ball";// Database name
+    private static SQLiteDatabase mDataBase;
+    private final Context mContext;
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, DB_NAME, null, 1);// 1? its Database Version
+        if(android.os.Build.VERSION.SDK_INT >= 4.2){
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        }
+        else
+        {
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
+        this.mContext = context;
+    }
+
+    public void createDataBase() throws IOException
+    {
+        //If database not exists copy it from the assets
+        boolean mDataBaseExist = checkDataBase();
+        if(!mDataBaseExist)
+        {
+            this.getReadableDatabase();
+            this.close();
+            try
+            {
+                //Copy the database from assests
+                copyDataBase();
+            }
+            catch (IOException mIOException)
+            {
+                Toast.makeText(mContext, "Fehler beim Kopieren der Datenbank", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    //Check that the database exists here: /data/data/your package/databases/Da Name
+    private boolean checkDataBase()
+    {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    //Copy the database from assets
+    private void copyDataBase() throws IOException
+    {
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer))>0)
+        {
+            mOutput.write(mBuffer, 0, mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
+    }
+
+    //Open the database, so we can query it
+    public boolean openDataBase() throws SQLException
+    {
+        String mPath = DB_PATH + DB_NAME;
+        //Log.v("mPath", mPath);
+        mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+
+        return mDataBase != null;
+    }
+
+    @Override
+    public synchronized void close()
+    {
+        if(mDataBase != null)
+            mDataBase.close();
+        super.close();
+    }
+
+    public static String GetColumnValue(Cursor cur, String ColumnName){
+        try {
+            return cur.getString(cur.getColumnIndex(ColumnName));
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+}
