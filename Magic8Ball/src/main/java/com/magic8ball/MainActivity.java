e1add64a0f04650907f3cc193de5215cd079db31
CarstenL <c.lockenkoetter@web.de>
2013-08-17 22:27:28 +0200

    Sprüche werden aus der DB gezogen; Menubar Button Event hinzugefügt, Menubar-Button per Laufzeit ein Icon zuweisen

--- Magic8Ball/src/main/java/com/magic8ball/MainActivity.java
+++ Magic8Ball/src/main/java/com/magic8ball/MainActivity.java
@@ -2,15 +2,22 @@ package com.magic8ball;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.magic8ball.util.SystemUiHider;
+
+import java.util.ArrayList;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -22,16 +29,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager sensorMgr;
     private Sensor accelerometer;
-
     private Vibrator vibrator;
-
     private float last_x = 0;
     private float last_y = 0;
     private float last_z = 0;
-
     private float interval = 300;
     private float threshold = 2000;
-
     private long lastUpdate;
 
     @Override
@@ -46,28 +49,33 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menubar, menu);
+        //set icon for the edit button
+        Drawable icon = getResources().getDrawable(R.drawable.ic_edit_groups);
+        menu.getItem(0).setIcon(icon);
+        return true;
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
     @Override
-    public final void onSensorChanged(SensorEvent event)
-    {
+    public final void onSensorChanged(SensorEvent event) {
         float x = event.values[0];
         float y = event.values[1];
         float z = event.values[2];
 
         long curTime = System.currentTimeMillis();
 
-        if ((curTime - lastUpdate) > 2000)
-        {
+        if ((curTime - lastUpdate) > 2000) {
             TextView tvQuestion = (TextView) findViewById(R.id.tvQuestion);
-            tvQuestion.setText("Frag deine Frage.");
 
-            float speed = StrictMath.abs(x+y+z-last_x-last_y-last_z)/interval * 10000;
-            if (speed > threshold)
-            {
+            float speed = StrictMath.abs(x + y + z - last_x - last_y - last_z) / interval * 10000;
+            if (speed > threshold) {
                 tvQuestion.setText("");
 
                 TextView tvSaying;
@@ -79,9 +87,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
                 vibrator.vibrate(200);
             }
-        }
-        else
-        {
+        } else {
             last_x = x;
             last_y = y;
             last_z = z;
@@ -89,45 +95,56 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         sensorMgr.unregisterListener(this);
     }
 
-    private String getSaying()
-    {
-        switch ((int)(StrictMath.random() *10))
-        {
-            case 0:
-                return getString(R.string.saying0);
-            case 1:
-                return getString(R.string.saying1);
-            case 2:
-                return getString(R.string.saying2);
-            case 3:
-                return getString(R.string.saying3);
-            case 4:
-                return getString(R.string.saying4);
-            case 5:
-                return getString(R.string.saying5);
-            case 6:
-                return getString(R.string.saying6);
-            case 7:
-                return getString(R.string.saying7);
-            case 8:
-                return getString(R.string.saying8);
-            case 9:
-                return getString(R.string.saying9);
-            default:
-                return "MOEP";
+    private String getSaying() {
+        ArrayList<String> checkedGroups = new ArrayList<String>();
+        //db öffnen
+        DataAdapter mDbHelper = new DataAdapter(this);
+        mDbHelper.createDatabase();
+        mDbHelper.open();
+
+        //alle gruppen ermitteln
+        Cursor allGroups = mDbHelper.getAllGroups();
+        //durchlaufe jede gruppe
+        while (!allGroups.isAfterLast()) {
+            String name = DatabaseHelper.GetColumnValue(allGroups, "NAME");
+
+            //prüfe ob gruppe erwünscht ist
+            Settings settings = new Settings(this);
+            if (settings.LoadPreferences(name)) {
+                checkedGroups.add(name);
+            }
+            allGroups.moveToNext();
+        }
+
+        //sprüche ermitteln
+        Cursor claims = mDbHelper.getClaims(checkedGroups);
+        mDbHelper.close();
+
+        //random nummer ermitteln
+        int randomNumber = (int) (StrictMath.random() * claims.getCount());
+
+        //cursor an die stelle der randomnummer springen
+        claims.moveToPosition(randomNumber - 1);
+
+        //spruch rückgabe
+        return DatabaseHelper.GetColumnValue(claims, "VALUE");
+    }
+
+    public void editConfig(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, Group.class);
+            startActivity(intent);
         }
     }
 }
